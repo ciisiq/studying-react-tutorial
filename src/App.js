3394e97890977cb81f5067c780562dd706d393c3
ciisiq <cii_3@hotmail.com>
2023-08-28 17:01:35 +0100

    add the first rule only one element

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import { useState } from 'react';
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+
+    if (squares[i]) {
+      return;
+    }
+
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
